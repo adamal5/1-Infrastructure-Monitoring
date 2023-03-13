4d3cb6add456086f38ea1c67842a011d4ee0b6cc
Adama <adamalorna@gmail.com>
2023-03-13 18:03:24 +0000

    threholds defined

--- infrastructure.py
+++ infrastructure.py
@@ -19,7 +19,11 @@ cpu_usage = psutil.cpu_percent(interval=1)
 memory_percentage_usage = psutil.virtual_memory().percent
 disk_usage = psutil.disk_usage('/').percent
 
+# Define the thresholds
 
+cpu_threshold = 30.0
+memory_threshold = 85.0
+disk_threshold = 50
 
 
 
